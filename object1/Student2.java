95aea28d5e734eee30d1b3fb179f2bf1b63133de
佐藤　祐也 <yuya02011991@icloud.com>
2016-07-22 18:02:17 +0900

    no message

---
+++ object1/Student2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.java;
+
+/**
+ *
+ * @author yuya
+ */
+public class Student2 extends Human {
+    public void a(){
+               age = 0;
+               height = 0;
+               }
+                 }
