fbe4928aa2d77a43ff4438239844f27a6998a30b
佐藤　祐也 <yuya02011991@icloud.com>
2016-08-08 11:44:49 +0900

    no message

---
+++ javaからのデータベース操作/dbaccess6.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author yuya
+ */
+public class dbaccess6 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *課題6:課題2でINSERTしたレコードを指定して削除してください。SELECT*で結果を表示してください
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+            
+        Connection db_con = null;
+        PreparedStatement db_st = null;
+        ResultSet db_data = null;
+       
+        try (PrintWriter out = response.getWriter()) {
+            
+                     try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                db_con = DriverManager.getConnection("jdbc:mysql://localhost:8889/challenge_db","sato","02011991");
+                db_st = db_con.prepareStatement("delete from profiles where name = ?");
+                db_st.setString(1, "名前");
+           
+                db_st.executeUpdate();
+                db_st.close();
+                db_con.close();
+                
+            }catch(SQLException e_sql){
+                out.println("接続時にエラーが発生しました:"+e_sql.toString());
+            }catch(Exception e){
+                out.println("接続時にエラーが発生しました:"+e.toString());
+            }finally{
+            if(db_con != null){
+                try{
+                db_con.close();                  
+            }catch(Exception e_con){
+                System.out.println(e_con.getMessage());
+                  }
+               }
+            }
+            
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet dbaccess6</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet dbaccess6 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
